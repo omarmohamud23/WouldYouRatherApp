57ae9fd3079409faf4eed4cd912cf7530702c8cc
Omar <Omar>
2020-11-09 15:06:05 -0600

    Would you rather app

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+ <h1>Would you rather...?</h1>
+
+<would-you-rather  v-bind:question="wyrQuestion"
+ v-bind:answer1="wyrAnswer1"
+ v-bind:answer2="wyrAnswer2" v-on:answer-changed="answerChanged"></would-you-rather>
+
+
+
+ <p>{{ userSelectionMessage }}</p>
+ 
+ 
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import WouldYouRather from './components/WouldYouRather.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    WouldYouRather
+  },
+  data(){
+    return{
+      wyrQuestion:' Would you rather be a wizard or a superhero?',
+      wyrAnswer1:'Wizard',
+      wyrAnswer2:'Superhero',
+      userSelectionMessage: ''
+    }
+  },
+  methods: {
+    answerChanged(choice)  {
+      this.userSelectionMessage = `Thanks! You chose ${choice}`
+    }
   }
 }
 </script>
@@ -24,5 +48,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background: bisque;
 }
 </style>
